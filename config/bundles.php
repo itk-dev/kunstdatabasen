113a4e18ff652ec43592e500aa463608b795da30
Troels Ugilt Jensen <tujensen@gmail.com>
2020-04-30 09:58:09 +0200

    SALG-919: Added encore

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     ItkDev\MockUpBundle\ItkDevMockUpBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
