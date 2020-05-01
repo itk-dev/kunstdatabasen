41045198dec5ec523dbba4c2909312f7bfa5fc93
Jesper Pedersen <pejes@aarhus.dk>
2020-05-01 14:38:27 +0200

    SALG-890: Collapsable advanced filters

--- assets/app/index.js
+++ assets/app/index.js
@@ -4,4 +4,5 @@ import './app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+// require('jquery')
 import 'bootstrap/js/dist/collapse'
