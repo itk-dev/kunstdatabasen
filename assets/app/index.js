fb26c63a470d33e74cff6871000a3f43db10ae28
Jesper Pedersen <pejes@aarhus.dk>
2020-05-01 11:56:59 +0200

    SALG-890: Add markup for frontend header and filters

--- assets/app/index.js
+++ assets/app/index.js
@@ -1,4 +1,7 @@
 import './app.scss';
+import './app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+import 'bootstrap/js/dist/collapse'
