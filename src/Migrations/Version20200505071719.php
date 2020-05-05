460e0edd0e9cc312013175e67ca1ec8376cc6784
Troels Ugilt Jensen <tujensen@gmail.com>
2020-05-05 10:26:34 +0200

    SALG-885: Added fixtures

--- src/Migrations/Version20200505071719.php
+++ src/Migrations/Version20200505071719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200504112648 extends AbstractMigration
+final class Version20200505071719 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200504112648 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE art_work (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, artist VARCHAR(255) NOT NULL, art_serial VARCHAR(255) NOT NULL, purchase_price DOUBLE PRECISION DEFAULT NULL, production_year INT DEFAULT NULL, assessment_date DATETIME DEFAULT NULL, assessment_price DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE artwork (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, artist VARCHAR(255) NOT NULL, art_serial VARCHAR(255) NOT NULL, purchase_price DOUBLE PRECISION DEFAULT NULL, production_year INT DEFAULT NULL, assessment_date DATETIME DEFAULT NULL, assessment_price DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200504112648 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE art_work');
+        $this->addSql('DROP TABLE artwork');
     }
 }
