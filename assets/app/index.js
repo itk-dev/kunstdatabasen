4ed51778ca87f1f7323c5fa3555e574036d1f6e4
Troels Ugilt Jensen <tujensen@gmail.com>
2020-05-12 12:55:25 +0200

    SALG-884: Added danish language

--- assets/app/index.js
+++ assets/app/index.js
@@ -15,13 +15,17 @@ dom.watch();
 import 'bootstrap/js/dist/collapse';
 
 require('select2');
+require('select2/dist/js/i18n/da');
 
 $(function () {
     $(document).ready(function () {
         $('.tag-select-edit').select2({
-            tags: true
+            tags: true,
+            language: 'da_DK'
         });
 
-        $('.tag-select').select2();
+        $('.tag-select').select2({
+            language: 'da_DK'
+        });
     });
 });
