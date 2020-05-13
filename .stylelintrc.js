940a82d2096a0f3d12b67f367b3833e16cc14f53
Troels Ugilt Jensen <tujensen@gmail.com>
2020-05-13 10:30:30 +0200

    SALG-884: Reverted style linting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,22 +1,5 @@
 module.exports = {
-    'extends': 'stylelint-config-recommended-scss',
-    'rules': {
-        'number-leading-zero': [
-            'never', {
-                'message': 'For consistency and to save a character don\'t use leading zeros on values less than 1'
-            }
-        ],
-        'string-quotes': [
-            'double', {
-                'message': 'For consistency use double quotes around strings'
-            }
-        ],
-        'block-opening-brace-space-before': 'always',
-        'declaration-block-trailing-semicolon': 'always',
-        'declaration-block-no-duplicate-properties': true,
-        'declaration-colon-space-after': 'always',
-        'no-duplicate-selectors': true,
-        'indentation': 4
-    },
+    '//': 'Do not add any custom rules. Stick with the guidelines layed out by https://sass-guidelin.es/',
+    'extends': 'stylelint-config-sass-guidelines',
     'ignoreFiles': ['vendor/**/*.*', 'node_modules/**/*.*']
 };
