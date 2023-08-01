11a1aa54645cf327618c46ac75fb72481bb48cae
Mikkel Ricky <rimi@aarhus.dk>
2023-08-01 15:41:26 +0200

    Updated recipe symfony/webpack-encore-bundle

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
     /*
@@ -26,9 +26,6 @@ Encore
     .addEntry('admin/new', './assets/admin/item/new.js')
     .addEntry('admin/edit', './assets/admin/item/edit.js')
     .addEntry('login', './assets/security/login.js')
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -50,14 +47,15 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+    // configure Babel
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
 
-    // enables @babel/preset-env polyfills
+    // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = 3;
+        config.corejs = '3.23';
     })
 
     // enables Sass/SCSS support
