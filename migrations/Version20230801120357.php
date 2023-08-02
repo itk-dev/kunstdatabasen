95c295cd042d46880604b04389960719ac66b477
Mikkel Ricky <rimi@aarhus.dk>
2023-08-02 14:39:49 +0200

    KUNST-71: Cleaned up migration

--- migrations/Version20230801120357.php
+++ migrations/Version20230801120357.php
@@ -19,14 +19,13 @@ final class Version20230801120357 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE migration_versions');
+        // The migration_versions table exists only if upgrading, i.e. not making a fresh install.
+        $this->addSql('DROP TABLE IF EXISTS migration_versions');
         $this->addSql('ALTER TABLE image ADD created_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE migration_versions (version VARCHAR(14) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, executed_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(version)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE image DROP created_at');
     }
